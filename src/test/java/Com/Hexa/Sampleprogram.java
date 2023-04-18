30e37427873d28e955919e2b5bb866b7b6e2e43b
DevendranRupesh <rupeshdevendran88@gmail.com>
2023-04-18 23:28:59 +0530

    my commit

---
+++ src/test/java/Com/Hexa/Sampleprogram.java
@@ -0,0 +1,36 @@
+package Com.Hexa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sampleprogram {
+	
+	public static void main(String[]args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions ops= new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(ops);
+		
+		driver.get("https://www.amazon.com/");
+		
+		driver.manage().window().fullscreen();
+		WebElement txtuser = driver.findElement(By.id("email"));
+		txtuser.sendKeys("greens");
+		
+		WebElement txtpass = driver.findElement(By.id("pass"));
+		txtpass.sendKeys("greens@123");
+		
+		WebElement btnlogin = driver.findElement(By.name("login"));
+		btnlogin.click();
+		
+		
+		
+	}
+
+}
